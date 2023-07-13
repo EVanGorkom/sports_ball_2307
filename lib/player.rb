c555aeeafc711ad591619bfb38a013db6a327ef1
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-07-13 10:27:34 -0500

    Create a player class

---
+++ lib/player.rb
@@ -0,0 +1,7 @@
+class Player 
+    def initialize(name, monthly_cost, contract_length)
+        @name = name
+        @monthly_cost = monthly_cost
+        @contract_length = contract_length
+    end
+end
