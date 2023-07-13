1ba38d9a3ebe8a849125013bcfefea218d3aa172
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-07-13 11:29:21 -0500

    Make progress on the add_player method and corresponding test

--- lib/team.rb
+++ lib/team.rb
@@ -1,18 +1,25 @@
 require './lib/player'
-# require './lib/team'
+require './lib/team'
 
 class Team
+    attr_accessor :roster
+
     def initialize(team_name, home_city)
         @team_name = team_name
         @home_city = home_city
     end
 
     def roster
-        roster = []
+        @roster = []
     end
 
     def player_count
         roster.length
+    end
+
+    def add_player(new_player)
+        @roster << new_player
+        require 'pry';binding.pry
     end
 
 end
